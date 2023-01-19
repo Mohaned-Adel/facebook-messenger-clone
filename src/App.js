033492dcea79817c445d09178eff831263744e91
Mohaned-Adel <mohaned.adel1996@gmail.com>
2023-01-19 15:23:07 +0200

    install material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // all the logic to send a message
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello from React 🚀!</h2>
+
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button disabled={!input} type="submit" onClick={sendMessage}>
+          Send Message
+        </Button>
+      </form>
+
+      {/* messages */}
+      {messages.map((message, index) => (
+        <p key={index}>{message}</p>
+      ))}
     </div>
   );
 }
